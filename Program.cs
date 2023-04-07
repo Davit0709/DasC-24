6b5b018c244aa395f28f99dd86d2c457f046f995
Davit <dav0709@icloud.com>
2023-04-07 19:02:51 +0400

    Uxagrum

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        PrintNumbersFromNTo1(n);
+    }
+
+    static void PrintNumbersFromNTo1(int n)
+    {
+        if (n == 0)
+        {
+            return;
+        }
+
+        Console.Write(n + " ");
+
+        PrintNumbersFromNTo1(n - 1);
+    }
+}
